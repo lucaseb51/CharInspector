f83601992c745de078f7f66cf30112529e7a3f13
Lucas G Eastburn <leastburn@mail.csuchico.edu>
2018-02-08 20:58:24 -0800

    Identifies capital Letters and ACII

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
-//Author:
+//Author: Lucas Eastburn
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
+char input;
 
   cout<<"What character do you want to know about?\n";
+  cin>>input;  
 
+
+  if(input>=65 && input<=90){
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout<<input<<" is an upper case letter!\n";
+  }
 
+  else if(input>=97 && input<=122){
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout<<input<<" is a lower case letter!\n";
+  }
 
+  else{
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
+  cout<<input<<"?! Pssh. What are you talking about?\n";
+}  
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
-
+  cout<<"The ASCII value is: "<<(int) input<<endl;
+  
   return 0;
 }
